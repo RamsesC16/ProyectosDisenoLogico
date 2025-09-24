0270011b384a8071edf47acb68772990ffaa6131
RamsesC16 <racortes@estudiantec.cr>
2025-09-24 09:44:42 -0600

    Bitácora

--- Proyecto1/src/design/module_decodi.sv
+++ Proyecto1/src/design/module_decodi.sv
@@ -3,7 +3,7 @@ module module_decodi (
     output logic [3:0] datos_out    // [i3,i2,i1,i0]
 );
 
-    // Extracción de bits de datos en orden clásico
+    // Extracción de bits de datos 
     assign datos_out[0] = datos_cod[2]; // i0 (d1)
     assign datos_out[1] = datos_cod[4]; // i1 (d2)
     assign datos_out[2] = datos_cod[5]; // i2 (d3)
